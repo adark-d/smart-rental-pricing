ddad13a243be61f33498349da584646081c445b8
Dave <davidwyse48@gmail.com>
2025-04-19 10:43:17 +0000

    Updated settings file

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -1,5 +1,15 @@
+from pathlib import Path
+
 from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix=False, settings_files=["config/settings.yaml"], load_dotenv=True
 )
+
+RAW_DIR = Path(settings.raw_data_dir)
+RAW_DIR.mkdir(parents=True, exist_ok=True)
+
+PROCESSED_DIR = Path(settings.processed_data_dir)
+PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
+
+use_thread = settings.use_thread
